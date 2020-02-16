f8b07113441a938a5d4cadf198fcbed238816ac7
Luis Rodrigues <luishumberto2000@gmail.com>
2020-02-16 05:00:52 -0300

    add: criacao de devs

--- src/models/Dev.js
+++ src/models/Dev.js
@@ -1,11 +1,17 @@
 const { Schema, model } = require("mongoose");
 
-const DevSchema = new Schema({
-  name: String,
-  github_username: String,
-  bio: String,
-  avatar_url: String,
-  techs: [String]
-});
+const DevSchema = new Schema(
+  {
+    name: String,
+    github_username: String,
+    bio: String,
+    avatar_url: String,
+    techs: [String]
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
 
 module.exports = model("Dev", DevSchema);
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,29 @@
 const { Router } = require("express");
+const axios = require("axios");
+const DevModel = require("./models/Dev");
 
 const routes = Router();
 
-routes.post("/devs", (req, res) => {
-  const { github_username } = req.body;
-  return res.json({ github_username });
+routes.post("/devs", async (req, res) => {
+  const { github_username, techs } = req.body;
+
+  const response = (
+    await axios.get(`https://api.github.com/users/${github_username}`)
+  ).data;
+
+  const { name = login, avatar_url, bio } = response;
+
+  const techsArray = techs.split(",").map(tech => tech.trim());
+
+  const dev = await DevModel.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray
+  });
+
+  return res.json(dev);
 });
 
 module.exports = routes;
